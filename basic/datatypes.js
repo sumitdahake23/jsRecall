b5593053af7abf0a22fedc350a954702b4d4dd53
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2024-06-06 16:23:42 +0000

    data types

---
+++ basic/datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) We are using node js Not Browser
+
+console.log(3 + 3);
+console.log("Sumit");
+
+// Code must be Redable Not Futuristic
+
+// Documentation of alert mdn 
+// tc39 Ecma International Documentation
+
+let name="Sumit" // String
+let age=19; //Integer
+let isLoggedIn=false; // Bollean
+
+// number = 2topower 53
+// BigInteger 
+// String =""
+// boolean=true/false
+
+// null -> standalone value
+// undefined ->yet to assign value
+// symbol -> to find uniquness
+
+// object => 
+
+console.log(typeof(age))
+
+console.log(typeof("Sumit"))
+
+console.log(typeof(null))
+
+console.log(typeof(undefined))

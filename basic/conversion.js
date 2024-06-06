4de1650dff2ad40e0ee1a47bb1ef8fa3d0b4a1c0
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2024-06-06 16:54:36 +0000

    Operations

--- basic/conversion.js
+++ basic/conversion.js
@@ -1,15 +1,59 @@
 // let score="32abc"
 let score = "33"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 // NaN not a number
 
 let isLoggedIn = 1;
 let booleanIsloggedIn=Boolean(isLoggedIn)
 
-console.log(booleanIsloggedIn)
+// console.log(booleanIsloggedIn)
+
+// *************** Operation ***************
+
+let value= 3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2="Sumit"
+
+let str3=str1+str2
+
+// console.log("1"+2+2)  122
+// console.log(1+2+"2") 32
+
+// console.log(+true) 1
+
+// console.log(+"") 0
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+// console.log(num2) 4
+
+let gameCounter = 100
+
+gameCounter++
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
+
+

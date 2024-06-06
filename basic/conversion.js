41eaae07e4ce600ca982f3a73f44656e7055b06e
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2024-06-06 16:38:50 +0000

    conversion of Data Types

---
+++ basic/conversion.js
@@ -0,0 +1,15 @@
+// let score="32abc"
+let score = "33"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+
+// NaN not a number
+
+let isLoggedIn = 1;
+let booleanIsloggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn)

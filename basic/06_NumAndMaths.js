78c7287d78ac6e293e52e6ed240f70dfdcff2f43
Sumit D. Dahake <77786015+sumitdahake23@users.noreply.github.com>
2024-08-04 19:42:48 +0000

    Number and Maths Important

---
+++ basic/06_NumAndMaths.js
@@ -0,0 +1,51 @@
+console.log("Sumit Dahake")
+
+const score=400;
+
+const balance = new Number(400)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// After this you can use any String Method
+
+// console.log(balance.toFixed(2))
+// After decimal how many digit it need eg 100 to 100.00
+
+// const otherNumber=523.3544
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds=100000;
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));// For Indian Number 
+
+// There are multiple values you can use With Number object with the help of browser you can read it
+
+// =======================================MATHS=============================================
+
+console.log(Math)
+
+// You will learn more about this object 
+
+console.log(Math.abs(-4))
+// To make negative value positive
+
+console.log(Math.round(4.6))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(4.6))
+
+// Mostly we used round only but remember about floor and ceil
+
+// Another methodds are min(Arg), max(arg), sqrt, power function is also there you have to learn about the same
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+// Important Formulae
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max - min +1)+min))
+
